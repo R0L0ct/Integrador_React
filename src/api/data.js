0ff8e5d1ffac32e692d15abe5dc41aae938a804a
R0L0ct <103770217+R0L0ct@users.noreply.github.com>
2023-08-25 17:19:41 -0300

    add api render link

--- src/api/data.js
+++ src/api/data.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const API = "http://localhost:3001/api/v1";
+//  const API = "http://localhost:3001/api/v1";
+const API = "https://integrador-backend.onrender.com";
 
 const axiosInstance = axios.create({
   withCredentials: true,
